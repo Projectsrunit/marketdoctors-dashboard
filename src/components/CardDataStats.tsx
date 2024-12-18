d622ed3d99e182ee294e1cbeea9c5a2c0d294c15
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-12-18 14:08:04 +0300

    Add total chews

--- src/components/CardDataStats.tsx
+++ src/components/CardDataStats.tsx
@@ -3,8 +3,6 @@ import React, { ReactNode } from "react";
 interface CardDataStatsProps {
   title: string;
   total: string;
-  rate: string;
-  levelUp?: boolean;
   levelDown?: boolean;
   children: ReactNode;
 }
@@ -12,8 +10,6 @@ interface CardDataStatsProps {
 const CardDataStats: React.FC<CardDataStatsProps> = ({
   title,
   total,
-  rate,
-  levelUp,
   levelDown,
   children,
 }) => {
@@ -33,26 +29,9 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({
 
         <span
           className={`flex items-center gap-1 text-sm font-medium ${
-            levelUp && "text-meta-3"
-          } ${levelDown && "text-meta-5"} `}
+            levelDown && "text-meta-5"
+          } `}
         >
-          {rate}
-
-          {levelUp && (
-            <svg
-              className="fill-meta-3"
-              width="10"
-              height="11"
-              viewBox="0 0 10 11"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M4.35716 2.47737L0.908974 5.82987L5.0443e-07 4.94612L5 0.0848689L10 4.94612L9.09103 5.82987L5.64284 2.47737L5.64284 10.0849L4.35716 10.0849L4.35716 2.47737Z"
-                fill=""
-              />
-            </svg>
-          )}
           {levelDown && (
             <svg
               className="fill-meta-5"
