243f18f7e1c2f3efd36a71da1e0402f9c3975f69
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2025-01-30 16:34:39 +0300

    Feature: Add Doctor Details

--- src/app/admin/doctor/[id]/page.tsx
+++ src/app/admin/doctor/[id]/page.tsx
@@ -10,7 +10,7 @@ import { toast } from "react-toastify";
 interface Doctor {
   id: number;
   full_name: string | null;
-  picture_url: string | null;
+  profile_picture: string | null;
   specialisation: string[];
   years_of_experience: number;
   confirmed: boolean;
@@ -19,11 +19,12 @@ interface Doctor {
   gender: string | null;
   home_address: string | null;
   languages: string[];
-  date_of_birth: string | null;
+  dateOfBirth: string | null;
   awards: string[];
   about: string | null;
   doctorId: number;
   consultation_fee: string | null;
+  facility: string | null;
 }
 
 interface InputFieldProps {
@@ -49,7 +50,7 @@ const InputField: React.FC<InputFieldProps> = ({
     </label>
     {isEditable ? (
       <input
-        type={name === "date_of_birth" ? "date" : "text"}
+        type={name === "dateOfBirth" ? "date" : "text"}
         name={name}
         value={value}
         onChange={onChange}
@@ -85,7 +86,7 @@ const DoctorSettingsPage = () => {
       const doctorData: Doctor = {
         id: result.id,
         full_name: `${result.firstName} ${result.lastName}`,
-        picture_url: result.profile_picture || "/default-avatar.png",
+        profile_picture: result.profile_picture || "/default-avatar.png",
         specialisation: result.specialisation ? [result.specialisation] : [],
         awards: result.awards ? result.awards : [],
         years_of_experience: Number(result.years_of_experience) || 0,
@@ -94,10 +95,11 @@ const DoctorSettingsPage = () => {
         gender: result.gender || "",
         home_address: result.home_address || "",
         languages: result.languages ? result.languages : [],
-        date_of_birth: result.dateOfBirth || "",
+        dateOfBirth: result.dateOfBirth || "",
         confirmed: result.confirmed || false,
         about: result.about || "",
         doctorId: result.id,
+        facility: result.facility,
         consultation_fee: result.consultation_fee || "",
       };
       setDoctor(doctorData);
@@ -138,11 +140,13 @@ const DoctorSettingsPage = () => {
       languages: Array.isArray(formData.languages)
         ? formData.languages.join(", ")
         : formData.languages,
-      date_of_birth: formData.date_of_birth,
+      dateOfBirth: formData.dateOfBirth,
       gender: formData.gender,
       years_of_experience: formData.years_of_experience,
       about: formData.about,
       consultation_fee: formData.consultation_fee,
+      facility: formData.facility,
+      awards: formData.awards,
     };
 
     console.log("formDataS", formDataS);
@@ -181,39 +185,95 @@ const DoctorSettingsPage = () => {
     <DefaultLayout>
       <div className="mx-auto max-w-270">
         <Breadcrumb pageName={`Doctor ${formData?.full_name || "Unknown"}`} />
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="space-y-6">
           <div className="overflow-hidden rounded-sm border border-stroke bg-white p-6 shadow-default dark:border-strokedark dark:bg-boxdark">
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
               {Object.entries({
+                "Profile Picture": "profile_picture",
+                "Full Name": "full_name",
                 Email: "email",
                 Phone: "phone",
                 Gender: "gender",
                 Specialisation: "specialisation",
                 Address: "home_address",
                 Languages: "languages",
-                "Date of Birth": "date_of_birth",
+                "Date of Birth": "dateOfBirth",
+                "Facility Name": "facility",
+                Background: "about",
                 Awards: "awards",
-                Experience: "years_of_experience",
-                "Consultation Fee": "consultation_fee",
-                "About Doctor": "about",
+
+                "Doctor Fee": "consultation_fee",
+                "Certifying Documents": "certify_url",
               }).map(([label, field]) => (
-                <InputField
-                  key={field}
-                  label={label}
-                  name={field}
-                  value={String(formData?.[field as keyof Doctor] || "")}
-                  onChange={handleChange}
-                  isEditable={true}
-                />
+                <div key={field} className="flex flex-col gap-2">
+                  <label className="text-sm font-medium text-black dark:text-white">
+                    {label}
+                  </label>
+
+                  {/* Profile Picture Handling */}
+                  {field === "profile_picture" ? (
+                    <div>
+                      {formData?.[field] ? (
+                        <img
+                          src={formData[field]}
+                          alt="Profile Picture"
+                          className="h-40 w-40 rounded border border-stroke object-cover dark:border-strokedark"
+                        />
+                      ) : (
+                        <span className="dark:text-gray-400 text-red">
+                          Doctor has no profile picture
+                        </span>
+                      )}
+                    </div>
+                  ) : /* Certifying Documents Handling */
+                  field === "certify_url" ? (
+                    // @ts-ignore
+                    formData?.[field] ? (
+                      <p className="font-medium text-primary underline">
+                        <a
+                          // @ts-ignore
+                          href={formData[field]}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          View Certifying Documents
+                        </a>
+                      </p>
+                    ) : (
+                      <p className="text-red">
+                        This user has no certifying documents.
+                      </p>
+                    )
+                  ) : /* Background (Text Area) Handling */
+                  field === "about" || field == "awards" ? (
+                    <textarea
+                      name={field}
+                      value={formData?.[field] || ""}
+                      onChange={handleChange}
+                      rows={5}
+                      className="w-full rounded border border-stroke bg-gray px-3 py-2 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                    />
+                  ) : (
+                    /* Default Input Field Handling */
+                    <input
+                      type="text"
+                      name={field}
+                      // @ts-ignore
+                      value={formData?.[field] || ""}
+                      onChange={handleChange}
+                      className="w-full rounded border border-stroke bg-gray px-3 py-2 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                    />
+                  )}
+                </div>
               ))}
             </div>
 
-            <div className="mt-6 flex justify-between">
+            <div className="mt-6 flex justify-end">
               <button
                 type="submit"
                 className="hover:bg-primarydark focus:bg-primarydark w-1/4 rounded bg-primary py-3 text-white focus-visible:outline-none"
               >
-                Update Profile
+                {loading ? "Updating..." : "Update Profile"}
               </button>
             </div>
           </div>
