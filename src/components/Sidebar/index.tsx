edba7c73b3620d35a9864ce0a352522a909a61be
Arafat Magezi <arafats144@gmail.com>
2025-02-06 17:39:37 +0300

    Added width to dashboard

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -227,16 +227,16 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
     <ClickOutside onClick={() => setSidebarOpen(false)}>
       <aside
-        className={`fixed left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:translate-x-0 ${
+        className={`fixed left-0 top-0 z-9999 flex h-screen w-60 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:translate-x-0 ${
           sidebarOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
         {/* <!-- SIDEBAR HEADER --> */}
-        <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
+        <div className="flex items-center justify-between gap-2 px-4 py-5.5 lg:py-6.5">
           <Link href="/">
             <Image
-              width={100}
-              height={20}
+              width={80}
+              height={16}
               src={"/images/logo/logo.png"}
               alt="Logo"
               priority
